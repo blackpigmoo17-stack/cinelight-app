96bfbfbbc20af2c2c5ffef0c16653871f3292306
blackpigmoo17-stack <blackpigmoo17@gmail.com>
2026-06-09 17:01:36 +0700

    feat: save equipment to localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useCallback } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import LightingDiagram3D from "./LightingDiagram3D";
 import EquipmentSelector from "./EquipmentSelector";
 
